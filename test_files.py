a009dff32c5b7a93dd71f6c307d99c7897a547bf
Latskovskiy <nikitadolgov96@mail.ru>
2023-12-17 15:42:46 +0300

    add changes

--- test_files.py
+++ test_files.py
@@ -22,10 +22,10 @@ def archive_files():
 
 
 def test_create_archive(archive_files):
-    assert zipfile.ZipFile("resources/test.zip") != None
+    assert zipfile.ZipFile("resources/test.zip") is not None
 
 
-def test_open_files(archive_files):
+def test_pdf_files(archive_files):
     with zipfile.ZipFile("resources/test.zip") as zip_file:
         print(zip_file.namelist())
         with zip_file.open("Python Testing with Pytest (Brian Okken).pdf") as pdf_file:
@@ -33,11 +33,17 @@ def test_open_files(archive_files):
             text = reader.pages[1].extract_text()
             assert "Python Testing with pytest" in text
 
+
+def test_csv_file(archive_files):
+    with zipfile.ZipFile("resources/test.zip") as zip_file:
         with zip_file.open("ololo.csv", "r") as csv_file:
             reader = list(csv.reader(TextIOWrapper(csv_file)))
             print(reader[1][0])
             assert "ololo" == reader[1][0]
 
+
+def test_xlsx_file(archive_files):
+    with zipfile.ZipFile("resources/test.zip") as zip_file:
         with zip_file.open("file_example_XLSX_50.xlsx") as xlsx:
             workbook = load_workbook(xlsx)
             sheet = workbook.active
